f64b83e85da83495f118a2a6609ef6bebaa1cb52
anubha1009 <axv64880@ucmo.edu>
2023-11-17 17:39:31 -0600

    Added Data Access Class / created Performance Review Tab in menu

---
+++ Group3HRManagementSystem/DataAccess.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Created By Anubha Vishwakarma
+namespace Group3HRManagementSystem
+{
+    internal class DataAccess
+    {
+        //declare connection
+        private SqlConnection connection;
+        //declare connection string 
+        private string connectionString = ConfigurationManager.ConnectionStrings[""].ConnectionString;
+
+        //setup connection
+        private SqlConnection GetConnection()
+        {
+            if (connection == null)
+            {
+                connection = new SqlConnection(connectionString);
+            }
+            return connection;
+        }//end Getconnection
+
+        private void OpenConnection()
+        {
+            if(connection.State == System.Data.ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+        }//end open connection
+
+
+        private void CloseConnection()
+        {
+            connection.Close();
+        }//end close Connection
+
+
+
+
+    }//end Data Access Class
+}//end namespace
