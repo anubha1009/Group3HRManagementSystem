16932ea188f8a2cb93ffaa0d28877e4316cf9dda
nehareddy31 <tunikinehareddy456@gmail.com>
2023-11-20 21:27:35 -0600

    created project view

--- Group3HRManagementSystem/Project View.Designer.cs
+++ Group3HRManagementSystem/Project View.Designer.cs
@@ -28,19 +28,142 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.searchProjectByNameTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.searchByIdTextBox = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.DescriptionLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(467, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Project View";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS Reference Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(193, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(167, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Search Project By Name";
+            // 
+            // searchProjectByNameTextBox
+            // 
+            this.searchProjectByNameTextBox.Location = new System.Drawing.Point(437, 102);
+            this.searchProjectByNameTextBox.Name = "searchProjectByNameTextBox";
+            this.searchProjectByNameTextBox.Size = new System.Drawing.Size(246, 22);
+            this.searchProjectByNameTextBox.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS Reference Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(193, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Search By ID";
+            // 
+            // searchByIdTextBox
+            // 
+            this.searchByIdTextBox.Location = new System.Drawing.Point(437, 151);
+            this.searchByIdTextBox.Name = "searchByIdTextBox";
+            this.searchByIdTextBox.Size = new System.Drawing.Size(246, 22);
+            this.searchByIdTextBox.TabIndex = 4;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(185, 212);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(116, 23);
+            this.searchButton.TabIndex = 5;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(409, 212);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(103, 23);
+            this.clearButton.TabIndex = 6;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(657, 212);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 7;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(84, 255);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1024, 203);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // DescriptionLabel
+            // 
+            this.DescriptionLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.DescriptionLabel.Location = new System.Drawing.Point(84, 471);
+            this.DescriptionLabel.Name = "DescriptionLabel";
+            this.DescriptionLabel.Size = new System.Drawing.Size(1024, 169);
+            this.DescriptionLabel.TabIndex = 9;
             // 
             // Project_View
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1188, 641);
+            this.Controls.Add(this.DescriptionLabel);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.searchByIdTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.searchProjectByNameTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "Project_View";
             this.Text = "ProjectView";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox searchProjectByNameTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox searchByIdTextBox;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label DescriptionLabel;
     }
 }
