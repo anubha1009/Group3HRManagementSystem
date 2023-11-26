d4af77c4900a795d29fb03a84c1bbe9f6107483d
anubha1009 <axv64880@ucmo.edu>
2023-11-25 22:18:38 -0600

    Project removed properties

--- Group3HRManagementSystem/Project.cs
+++ Group3HRManagementSystem/Project.cs
@@ -12,9 +12,6 @@ namespace Group3HRManagementSystem
         public int ProjectId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public DateTime StartDate { get; set; }
-        public DateTime? EndDate { get; set; }
-        public string Status { get; set; }
         public int HoursAllocated { get; set; }
         public double Budget { get; set; }
 
